2208856d20ccda499138d82c0dd42e0d11f74434
Elias Braz <elias.brazjunior@gmail.com>
2019-10-27 16:24:41 -0300

    Correçao de server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  urlSpring: 'https://butler-api.herokuapp.com'
+  urlSpring: 'https://butler-api-prod.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  urlSpring: 'https://butler-api.herokuapp.com'
+  urlSpring: 'https://butler-api-dev.herokuapp.com'
 };
 
 /*
