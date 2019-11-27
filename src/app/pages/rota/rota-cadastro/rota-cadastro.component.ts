116012b6d076510333c3a62b2bdfb42f4a86e21e
Elias Braz <elias.brazjunior@gmail.com>
2019-11-27 10:31:05 -0300

    Incluindo validações no formulário de rota

--- src/app/pages/rota/rota-cadastro/rota-cadastro.component.ts
+++ src/app/pages/rota/rota-cadastro/rota-cadastro.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Rota } from '../../../models/rota';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { DefaultService } from '../../../services/default.service';
 import { ToastService } from '../../../services/toast.service';
 import { SharedService } from 'src/app/shared/shared.service';
 import { NgxSpinnerService } from 'ngx-spinner';
-
-import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-rota-cadastro',
@@ -18,12 +15,11 @@ export class RotaCadastroComponent implements OnInit {
 
   rota: Rota = new Rota();
 
-  formulario: FormGroup;
+  isSubmit: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private defaultService: DefaultService,
-    private formBuilder: FormBuilder,
     private cdr: ChangeDetectorRef,
     private sharedService: SharedService,
     private toastService: ToastService,
@@ -36,11 +32,6 @@ export class RotaCadastroComponent implements OnInit {
         this.rota.id = params['id'];
         this.getById();
       }
-    });
-
-    this.formulario = this.formBuilder.group({
-      ativo: ['', [Validators.required]],
-      nome: ['', [Validators.required, Validators.min(1), Validators.max(40)]]
     });
   }
 
@@ -58,13 +49,10 @@ export class RotaCadastroComponent implements OnInit {
     }, () => this.spinner.hide())
   }
 
-  isValid(field) {
-    return this.formulario.get(field).status == 'VALID' ? true : false;
-  }
-
-  salvar() {
-    if (this.formulario.invalid) {
-      Swal.fire('Cadastro de rota', 'Não é possível salvar o rota!<br>Existem campos inválidos', 'error');
+  salvar(form) {
+    if (form.invalid) {
+      this.isSubmit = true;
+      return;
     } else {
       this.rota.usuario = this.sharedService.getUsuarioLogged();
 
