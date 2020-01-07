03ccca5ce997a1880da1743f71afd1478a9424d2
Elias Braz <elias.brazjunior@gmail.com>
2020-01-06 22:51:15 -0300

    Configurando Procfile para deploy no Heroku

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  urlSpring: 'https://butler-api-dev.us-east-1.elasticbeanstalk.com'
+  urlSpring: 'https://butler-api-dev.herokuapp.com'
 };
 
 /*
