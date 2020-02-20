228bff0a3aced47dc12156af0f46fec5287d906f
Elias Braz <elias.brazjunior@gmail.com>
2020-02-19 23:42:30 -0300

    Mudança de cor padrao para purple

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -4,7 +4,7 @@ export class GradientConfig {
     subLayout: '', // horizontal-2
     collapseMenu: false,
     layoutType: 'menu-dark', // menu-dark, menu-light, dark
-    headerBackColor: 'header-dark', // header-blue, header-red, header-purple, header-info, header-dark, header-orenge
+    headerBackColor: 'header-purple', // header-blue, header-red, header-purple, header-info, header-dark, header-orenge
     // header-green, header-yellow, header-orchidgreen, header-indigogreen, header-darkgreen, header-darkblue
     rtlLayout: false,
     navFixedLayout: true,
