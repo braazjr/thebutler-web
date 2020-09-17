6c9d9d2acb92e4a804e2c5083aab665a308ba453
Elias Braz <elias.brazjunior@gmail.com>
2020-09-17 20:48:14 -0300

    Atualização no nome do objeto

--- src/app/pages/condominio/condominio-cadastro/condominio-cadastro.component.ts
+++ src/app/pages/condominio/condominio-cadastro/condominio-cadastro.component.ts
@@ -88,7 +88,7 @@ export class CondominioCadastroComponent implements OnInit {
       this.isSubmit = true;
       return;
     } else {
-      this.condominio['idEmpresa'] = Number(this.empresaId);
+      this.condominio['empresaId'] = this.empresaId;
 
       if (!this.condominio.id) {
         this.defaultService.salvar('condominios', this.condominio)
