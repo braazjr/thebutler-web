72a9ef2e58db07c5b518ded7e248c3da16d491ff
Elias Braz <elias.brazjunior@gmail.com>
2020-12-29 20:17:06 -0300

    Back page button

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  urlAuthSpring: 'http://localhost:8080/oauth',
-  urlSpring: 'http://localhost:8080/api'
+  urlAuthSpring: 'https://api.thebutler.com.br/oauth',
+  urlSpring: 'https://api.thebutler.com.br/api'
 };
 
 /*
