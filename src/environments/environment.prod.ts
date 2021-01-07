00fcb4e4e523c7869c899f0871756b2c13f9f6eb
Elias Braz <elias.brazjunior@gmail.com>
2021-01-07 11:17:51 -0300

    Atualizaçao na url da api em prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlAuthSpring: 'https://api.thebutler.com.br/oauth',
-  urlSpring: 'https://api.thebutler.com.br/api'
+  urlAuthSpring: 'https://thebutler-api.herokuapp.com/oauth',
+  urlSpring: 'https://thebutler-api.herokuapp.com/api'
 };
